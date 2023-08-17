bf3984b741dea67805dd75ddf4f3af6d88dc6a6c
Maxim Shmelkov <1002@tavr.su>
2023-08-17 17:19:37 +0400

    добавлен prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,16 @@ module.exports = {
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
+  plugins : ["prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": 'off'
+    "vue/multi-word-component-names": 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
   },
 };
