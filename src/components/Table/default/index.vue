c000f86abb04ae5c9f14ced684b04f99ece97dc0
Aleksey Tihonravov <fosterfosterfoster63@gmail.com>
2023-08-17 18:00:25 +0400

    start clear prettier

--- src/components/Table/default/index.vue
+++ src/components/Table/default/index.vue
@@ -1,38 +1,38 @@
 <template>
   <div class='v-table'>
-    <div :class='options.options.headerFixed ? 'v-table-panel--fixed' : ''' class='v-table-panel'>
+    <div :class='options.options.headerFixed ? "v-table-panel--fixed" : ""' class='v-table-panel'>
       <div class='v-table-panel__actions'>
           <v-button :option='button' v-for='(button, indexButton) in options.panel.buttons' :key='indexButton'></v-button>
           <!--<v-button :option='button' v-for='(button, indexButton) in options.panel.buttons' :key='indexButton'></v-button>-->
       </div>
       <div class='v-table-panel__search'>
-        <v-input @clearfield='clearField('searchField')' clearing type='search' placeholder='Поиск' v-model='searchField'></v-input>
+        <v-input @clearfield="clearField('searchField')" clearing type='search' placeholder='Поиск' v-model='searchField'></v-input>
       </div>
     </div>
     <table id='mainTable' ref='mainTable' class='v-table-wrap'>
-      <thead :class='options.options.headerFixed ? 'v-table-header--fixed' : ''' class='v-table-header'>
+      <thead :class='options.options.headerFixed ? "v-table-header--fixed" : ""' class='v-table-header'>
         <tr class='v-table-header-row'>
           <th align='center' v-if='options.options.selecting' width='5%'>
             <!--s-->
           </th>
-          <th :style='{ left: getWidth(head.value) }' :align='head.align' :class='head.fixed ? 'v-table-header-row-cell--fixed' : ''' v-show='true' :id='head.value + '-table-header'' :width='head.width' class='v-table-header-row-cell' v-for='(head, index) in options.head' :key='index'>
+          <th :style='{ left: getWidth(head.value) }' :align='head.align' :class='head.fixed ? "v-table-header-row-cell--fixed" : ""' v-show='true' :id="head.value + '-table-header'" :width='head.width' class='v-table-header-row-cell' v-for='(head, index) in options.head' :key='index'>
             <div class='v-table-header-row-cell-wrap'>
               <span @click='openSort(head)'>{{ head.title }}</span>
-              <transition name='accordion'>
+              <transition name="accordion">
                 <div v-if='head.sorts && head.sorts[0].isShow' class='v-table-header-row-cell-sort'>
-                  <div @click='sortRow(head)' class='v-table-header-row-cell-sort__row' v-if='head.sorts[0].type === 'string''>
+                  <div @click='sortRow(head)' class='v-table-header-row-cell-sort__row' v-if="head.sorts[0].type === 'string'">
                     <v-icon-sort :state='head.sorts[0].value'/>
                     <p v-if='true'>Сортировка от А до Я</p>
                   </div>
-                  <div @click='sortRow(head)' class='v-table-header-row-cell-sort__row' v-if='head.sorts[0].type === 'number''>
+                  <div @click='sortRow(head)' class='v-table-header-row-cell-sort__row' v-if="head.sorts[0].type === 'number'">
                     <v-icon-sort :state='head.sorts[0].value'/>
                     <p v-if='true'>Сортировка по убыванию</p>
                   </div>
-                  <div @click='sortRow(head)' class='v-table-header-row-cell-sort__row' v-if='head.sorts[0].type === 'date''>
+                  <div @click='sortRow(head)' class='v-table-header-row-cell-sort__row' v-if="head.sorts[0].type === 'date'">
                     <v-icon-sort :state='head.sorts[0].value'/>
                     <p v-if='true'>Сортировка по дате</p>
                   </div>
-                  <v-input class='v-table-header-row-cell-sort__search' @clearfield='clearField('searchField')' clearing type='search' placeholder='Поиск' v-model='head.search.field'></v-input>
+                  <v-input class='v-table-header-row-cell-sort__search' @clearfield="clearField('searchField')" clearing type='search' placeholder='Поиск' v-model='head.search.field'></v-input>
                 </div>
               </transition>
             </div>
@@ -40,9 +40,9 @@
           <!--<th class='v-table-header-row-cell' v-for='(head, index) in options.head'>{{ head.title }}</th>-->
         </tr>
       </thead>
-      <tbody class='v-table-body'>
+      <tbody class="v-table-body">
         <template v-for='(row, indexRow) in options.data.rows' >
-          <tr :key='indexRow' :class='row.row.selected ? 'v-table-body-row--selected' : '' ' @contextmenu='openContext($event, row)' @click='openChildRow($event, row)' class='v-table-body-row'>
+          <tr :key='indexRow' :class='row.row.selected ? "v-table-body-row--selected" : "" ' @contextmenu='openContext($event, row)' @click='openChildRow($event, row)' class='v-table-body-row'>
             <td class='v-table-body-row-cell__checkbox'  align='center' v-if='options.options.selecting' width='5%'>
               <div @click.stop class='v-table-checkbox'>
                 <label>
@@ -52,11 +52,11 @@
                 </label>
               </div>
             </td>
-            <td :style='{ left: getWidth(cell.value, cell.value + '-table-cell' + '_id' + row.row.id) }' :class='cell.fixed ? 'v-table-body-row-cell--fixed' : ''' :id='cell.value + '-table-cell' + '_id' + row.row.id' :align='cell.align' class='v-table-body-row-cell v-table-actions' v-show='cell.isShow ? true : false' v-for='(cell, cellIndex) in options.head' :key='cellIndex'>
-              <template v-if='cell.type === 'default''>
+            <td :style='{ left: getWidth(cell.value, cell.value + "-table-cell" + "_id" + row.row.id) }' :class='cell.fixed ? "v-table-body-row-cell--fixed" : ""' :id='cell.value + "-table-cell" + "_id" + row.row.id' :align="cell.align" class='v-table-body-row-cell v-table-actions' v-show='cell.isShow ? true : false' v-for='(cell, cellIndex) in options.head' :key='cellIndex'>
+              <template v-if="cell.type === 'default'">
                 {{ row.row[cell.value] }}
               </template>
-              <template v-else-if='cell.type === 'actions''>
+              <template v-else-if="cell.type === 'actions'">
                 <v-table-button :row='row.row' :option='action' v-for='(action, indexAction) in cell.actions' :key='indexAction'/>
               </template>
             </td>
@@ -64,8 +64,8 @@
           <tr :key='indexRow' v-show='row.child.isShow && options.head.some(el => !el.isShow)' class='v-table-body-row v-table-body-row--child overflowHidden'>
             <td class='v-table-body-row-cell' :colspan='colspanLength'>
               <transition-group  name='testanim' class='overflowHidden' tag='ul'>
-                <template v-if='!cell.isShow' v-for='cell, cellIndex in options.head'>
-                  <li v-if='cell.type === 'default'' class='v-table-body-row-paragraph' :key='cellIndex'>
+                <template v-if="!cell.isShow" v-for="cell, cellIndex in options.head">
+                  <li v-if="cell.type === 'default'" class='v-table-body-row-paragraph' :key='cellIndex'>
                     <span>{{ cell.title }}: </span> <span>{{ row.child.data[cell.value] }}</span>
                   </li>
                   <li v-else-if='cell.type === 'actions'' class='v-table-body-row-paragraph v-table-actions' :key='cellIndex'>
@@ -126,7 +126,7 @@
         </div>
       </div>
     </div>
-    <v-contextmenu :options='contextmenu'/>
+    <v-contextmenu :options="contextmenu"/>
   </div>
 </template>
 
