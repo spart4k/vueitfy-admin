6b53f5b18ed78edd2410337c6c15d513e6e3e9a2
Aleksey Tihonravov <fosterfosterfoster63@gmail.com>
2023-09-06 15:08:08 +0400

    fix css includes files

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -1,31 +1,27 @@
 <template>
-  <div>
-    <div class="d-flex flex-grow-1">
-      <div v-if="!isMobile">
-        <Nav></Nav>
-      </div>
-      <div
-        v-if="isMobile"
-        id="mobileMenu"
-        :class="
-          isOpenMenu
-            ? 'mobile-menu__window--open'
-            : 'mobile-menu__window--close'
-        "
-      >
-        <Nav></Nav>
-      </div>
-      <div
-        id="bg__black"
-        :class="isOpenMenu ? 'bg__black--active' : 'bg__black'"
-      ></div>
-      <div class="d-flex flex-column flex-grow-1">
-        <Topbar></Topbar>
-        <v-main class="d-flex flex-column flex-grow-1">
-          <slot></slot>
-          <!--<Alert />-->
-        </v-main>
-      </div>
+  <div class="layout-default d-flex flex-grow-1">
+    <div v-if="!isMobile">
+      <Nav></Nav>
+    </div>
+    <div
+      v-if="isMobile"
+      id="mobileMenu"
+      :class="
+        isOpenMenu ? 'mobile-menu__window--open' : 'mobile-menu__window--close'
+      "
+    >
+      <Nav></Nav>
+    </div>
+    <div
+      id="bg__black"
+      :class="isOpenMenu ? 'bg__black--active' : 'bg__black'"
+    ></div>
+    <div class="d-flex flex-column flex-grow-1">
+      <Topbar></Topbar>
+      <v-main class="d-flex flex-column flex-grow-1">
+        <slot></slot>
+        <!--<Alert />-->
+      </v-main>
     </div>
   </div>
 </template>
--- src/views/testform.vue
+++ src/views/testform.vue
@@ -10,13 +10,13 @@
 
 <!--<style lang="scss" src="@/trash/nav.scss"></style>-->
 
-<style>
-@import 'https://personal-crm.ru/js/vendor/bootsrap5.1.3/css/bootstrap.min.css';
-</style>
+<!-- <style>
+// @import 'https://personal-crm.ru/js/vendor/bootsrap5.1.3/css/bootstrap.min.css';
+</style> -->
 
-<style src="@/trash/main.css"></style>
-<style src="@/trash/override.css"></style>
-<style src="@/trash/choosen.min.css"></style>
+<style scoped src="@/trash/main.css"></style>
+<style scoped src="@/trash/override.css"></style>
+<style scoped src="@/trash/choosen.min.css"></style>
 
 <!--<script src="https://personal-crm.ru/js/vendor/jquery/jquery-with-common-plugins.js"></script>-->
 <!--<script src="@/trash/main.js"></script>-->
