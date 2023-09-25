3f303d46c00cc5963c4ae30e6d446156c857e1ac
Dmitrij <tolstov.dmitrij42@gmail.com>
2023-09-25 09:18:44 +0400

    fix: route?.query

--- src/components/Mails/letter/expanded/index.vue
+++ src/components/Mails/letter/expanded/index.vue
@@ -4,11 +4,11 @@
       'v-letter-expanded',
       'd-flex',
       'flex-column',
-      $route.query.mail && 'v-letter-expanded__edited',
+      $route?.query?.mail && 'v-letter-expanded__edited',
     ]"
   >
     <div class="v-letter-expanded-user">
-      <template v-if="!$route.query.compose">
+      <template v-if="!$route?.query?.compose">
         <v-icon class="v-letter-expanded-user_icon" small>$IconBookmark</v-icon>
         <MailsLetterUser expanded />
         <div class="v-letter-expanded-user-favorite">
@@ -26,19 +26,21 @@
       <MailsLetterUserEdit v-else />
     </div>
     <div class="v-letter-expanded-container">
-      <MailsLetterTextEdit v-if="$route.query.compose" />
+      <MailsLetterTextEdit v-if="$route?.query?.compose" />
       <MailsLetterText
-        v-if="$route.query.compose !== 'new'"
-        :edit="!$route.query.compose || $route.query.compose === 'edit'"
+        v-if="$route?.query?.compose !== 'new'"
+        :edit="!$route?.query?.compose || $route?.query?.compose === 'edit'"
       />
     </div>
     <div class="v-letter-expanded_btn pb-2 mt-4">
       <v-btn
-        @click="$route.query.compose === 'new' ? createMail() : answerToMail()"
+        @click="
+          $route?.query?.compose === 'new' ? createMail() : answerToMail()
+        "
         color="primary"
       >
         <v-icon small class="mr-2">$IconEdit</v-icon>
-        {{ $route.query.compose === 'new' ? 'Отправить' : 'Ответить' }}
+        {{ $route?.query?.compose === 'new' ? 'Отправить' : 'Ответить' }}
       </v-btn>
     </div>
   </div>
