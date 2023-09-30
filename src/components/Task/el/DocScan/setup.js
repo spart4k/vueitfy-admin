29ea7b6f0403967f6864fd996e196f6dd7ebf0db
korovkindm <korovkindm@63pokupki.ru>
2023-09-30 18:40:17 +0400

    add FormError and fix DocScan

--- src/components/Task/el/DocScan/setup.js
+++ src/components/Task/el/DocScan/setup.js
@@ -1,7 +1,11 @@
 import { defineComponent } from 'vue'
+import FormError from '../FormError/index.vue'
 
 const docScan = defineComponent({
   name: 'DocScan',
+  components: {
+    FormError,
+  },
   props: {},
   setup() {
     return {}
---
+++ src/components/Task/el/FormError/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-row class="form-error">
+    <span class="form-error__accent">!</span>
+    <slot></slot>
+  </v-row>
+</template>
+
+<script src="./setup.js"></script>
+
+<style lang="scss" scoped src="./style.scss"></style>
