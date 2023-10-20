5a5ed948b60dc35fd364768677d7870d55112d29
Dmitrij <tolstov.dmitrij42@gmail.com>
2023-10-20 15:13:59 +0400

    fix: message read

--- src/api/mail.js
+++ src/api/mail.js
@@ -12,41 +12,39 @@ export const getMail = (id) => get(`mail/get_message/${id}`)
 
 export const getSendedMessage = (id) => get(`mail/get_send_message/${id}`)
 
-export const createBox = (data) => post('mail/create_box', { body: data })
+export const createBox = (data) => post('mail/create_box', data)
 
-export const createFolder = (data) => post('mail/create_folder', { body: data })
+export const createFolder = (data) => post('mail/create_folder', data)
 
 export const sendMessage = (data, config) =>
   post('mail/send_message', data, config)
 
 export const getSendedMessages = (data, params) =>
-  post(`mail/get_send_messages/box/${params}`, { body: data })
+  post(`mail/get_send_messages/box/${params}`, data)
 
 export const getDeletedMessages = (data, params) =>
-  post(`mail/get_messages/del/${params}`, { body: data })
+  post(`mail/get_messages/del/${params}`, data)
 
 export const getBoxMails = (data, params) =>
-  post(`mail/get_messages/box/${params}`, { body: data })
+  post(`mail/get_messages/box/${params}`, data)
 
 export const getFolderMails = (data, params) =>
-  post(`mail/get_messages/folder/${params}`, { body: data })
+  post(`mail/get_messages/folder/${params}`, data)
 
-export const filterMail = (data) => post('mail/update_messages', { body: data })
+export const filterMail = (data) => post('mail/update_messages', data)
 
-export const countFilter = (data, type) =>
-  post(`mail/count/${type}`, { body: data })
+export const countFilter = (data, type) => post(`mail/count/${type}`, data)
 
-export const editBox = (data, params) =>
-  put(`mail/update_box/${params}`, { body: data })
+export const editBox = (data, params) => put(`mail/update_box/${params}`, data)
 
 export const editFolder = (data, params) =>
-  put(`mail/update_folder/${params}`, { body: data })
+  put(`mail/update_folder/${params}`, data)
 
 export const changeMail = (data, params) =>
-  put(`mail/update_message/${params}`, { body: data })
+  put(`mail/update_message/${params}`, data)
 
 export const changeLettersContainer = (data, type, id) =>
-  put(`mail/change/${type}/${id}`, { body: data })
+  put(`mail/change/${type}/${id}`, data)
 
 export const deleteBox = (data) => del(`mail/box/${data}`)
 
