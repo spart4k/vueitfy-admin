1ee015936b589556c7de9cf92b3ba5a268fade74
Grigory-F <grishka2500@gmail.com>
2023-11-02 01:03:21 +0400

    awdawd

--- src/components/Task/form8/index.vue
+++ src/components/Task/form8/index.vue
@@ -21,77 +21,92 @@
         </v-col>
       </v-row>
       <!-- {{ getNameDoc(1) }} -->
-      <v-row>
-        <v-col>
-          <div class="mb-10">
-            <span style="font-size: 20px">Приложите документы</span>
-            <v-expansion-panels>
-              <v-expansion-panel
-                v-for="(item, index) in listDocuments"
-                :key="index"
-              >
-                <v-expansion-panel-header>
-                  <v-row align="center">
-                    <span>
-                      <v-icon left v-if="!item.inProcess"> $IconGalka </v-icon>
-                      <v-icon left v-if="item.inProcess"> $IconSetting </v-icon>
-                      {{ data.data.docs_spr[item.doc_id] }}
-                    </span>
-                  </v-row>
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  <Dropzone
-                    :options="{
-                      withoutSave: false,
-                      folder: 'tmp',
-                      removeble: false,
-                    }"
-                    :paramsForEmit="{ item: item.doc_id }"
-                    @addFiles="addFiles"
-                  ></Dropzone>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </div>
-
-          <v-divider></v-divider>
-        </v-col>
-      </v-row>
+      <div class="mb-10">
+        <span>Приложите документы</span>
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="(item, index) in listDocuments"
+            :key="index"
+          >
+            <v-expansion-panel-header>
+              <span>
+                <v-icon left v-if="!item.inProcess"> $IconGalka </v-icon>
+                <v-icon left v-if="item.inProcess"> $IconSetting </v-icon>
+                {{ data.data.docs_spr[item.doc_id] }}
+              </span>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <Dropzone
+                :options="{
+                  withoutSave: false,
+                  folder: 'tmp',
+                  removeble: false,
+                }"
+                :paramsForEmit="{ item: item.doc_id }"
+                @addFiles="addFiles"
+              ></Dropzone>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
 
       <v-row>
         <v-col cols="12">
           <div style="display: flex; justify-content: center">
-            <v-btn color="success"> Приложить </v-btn>
+            <v-btn
+              color="success"
+              :disabled="listDisbledDocuments != 0"
+              @click="sendDocuments"
+            >
+              Приложить
+            </v-btn>
           </div>
         </v-col>
       </v-row>
+      <div>
+        <span>Патент</span>
+      </div>
       <v-row>
-        <v-col cols="6">
+        <v-col
+          cols="6"
+          :class="[listDisbledDocuments > 0 ? 'overflow-inputs' : '']"
+        >
           <Dropzone
             :options="{
               withoutSave: false,
               folder: 'tmp',
+              removeble: false,
             }"
+            :paramsForEmit="{ item: 5 }"
+            @addFiles="addFilesPatent"
           ></Dropzone>
         </v-col>
-        <v-col cols="6">
+        <v-col
+          cols="6"
+          :class="[listDisbledDocuments > 0 ? 'overflow-inputs' : '']"
+        >
           <Dropzone
             :options="{
               withoutSave: false,
               folder: 'tmp',
+              removeble: false,
             }"
+            :paramsForEmit="{ item: 15 }"
+            @addFiles="addFilesPatent"
           ></Dropzone>
         </v-col>
       </v-row>
       <v-row class="py-2" justify="end">
-        <v-btn color="info" class="mr-3">
+        <v-btn
+          color="info"
+          class="mr-3"
+          :disabled="disableFinishState !== 2"
+          @click="sendTaskFinish"
+        >
           <v-icon left> $IconMain </v-icon>
           Завершить
         </v-btn>
-        <v-btn color="blue-grey">
-          <v-icon left> $IconMain </v-icon>
-          Закрыть
-        </v-btn>
+        <v-btn color="blue-grey"> Закрыть </v-btn>
       </v-row>
     </div>
   </div>
@@ -99,4 +114,18 @@
 
 <script src="./setup.js"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.overflow-inputs {
+  position: relative;
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    background-color: rgba($color: #000000, $alpha: 0.1);
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+</style>
--- src/components/Task/form8/setup.js
+++ src/components/Task/form8/setup.js
@@ -30,8 +30,8 @@ const Form8 = defineComponent({
     // onMounted(() => {
     //   console.log(docs_spr, getNameDoc)
     // })
-    let aaaaa = 1
     let listDocuments = ref([])
+    let listDisbledDocuments = ref(0)
 
     onMounted(() => {
       data.data.docs_grajdanstvo.forEach((item, index) => {
@@ -41,13 +41,15 @@ const Form8 = defineComponent({
         } else {
           pasteObject = { doc_id: item }
           pasteObject['inProcess'] = true
+          listDisbledDocuments.value = listDisbledDocuments.value + 1
         }
         listDocuments.value.push(pasteObject)
       })
     })
 
-    let listRequestsForUpload = []
+    let listRequestsForUpload = ref([])
     let file = ref('')
+    let disableFinishState = ref(0)
 
     // const sendData = () => {
     //   console.log(selectName.value, file.value)
@@ -114,18 +116,197 @@ const Form8 = defineComponent({
     //   pushSomeShit()
     //   changeStatus()
     // }
+    let addFilesPatent = (e, options) => {
+      let fileExt = e[0].type.split('/')[1]
+      let fileName = `personal_doc_` + Date.now() + '.' + fileExt
+      let form_data = new FormData()
+      form_data.append('file', e[0])
 
-    let addFiles = (e, awdwdw) => {
-      // file.value = e[0]
-      console.log(e, awdwdw)
+      const { makeRequest: updateFileData } = useRequest({
+        context,
+        request: () =>
+          store.dispatch('taskModule/updateFileData', {
+            personal_id: data.entity.id,
+            doc_id: e.item,
+            path_doc: `/files/personal_doc/${fileName}`,
+            from_task: true,
+          }),
+      })
+
+      const { makeRequest: loadImage } = useRequest({
+        context,
+        request: () =>
+          store.dispatch('taskModule/loadImage', {
+            id: 1,
+            folder: 'personal_doc',
+            fileName: fileName,
+            file: form_data,
+          }),
+        successMessage: 'Файл успешно загружен',
+      })
+      updateFileData()
+      loadImage()
+      disableFinishState.value = disableFinishState.value + 1
     }
 
-    const sendData = () => {}
+    let addFiles = (e, options) => {
+      console.log(e, options, listDocuments.value)
+
+      let fileExt = e[0].type.split('/')[1]
+      let fileName = `personal_doc_` + Date.now() + '.' + fileExt
+      let form_data = new FormData()
+      form_data.append('file', e[0])
+
+      let currentDropzone = listDocuments.value.find((x) => x.doc_id === e.item)
+
+      const { makeRequest: delInfoAFile } = useRequest({
+        context,
+        request: () =>
+          store.dispatch('taskModule/updateFileData', { id: e.item, del: 1 }),
+      })
+      const { makeRequest: updateFileData } = useRequest({
+        context,
+        request: () =>
+          store.dispatch('taskModule/updateFileData', {
+            personal_id: data.entity.id,
+            doc_id: e.item,
+            path_doc: `/files/personal_doc/${fileName}`,
+            from_task: true,
+          }),
+      })
+
+      const { makeRequest: loadImage } = useRequest({
+        context,
+        request: () =>
+          store.dispatch('taskModule/loadImage', {
+            id: 1,
+            folder: 'personal_doc',
+            fileName: fileName,
+            file: form_data,
+          }),
+        successMessage: 'Файл успешно загружен',
+      })
+
+      // Когда запрос будет готов от Миши, нужно сформировать его по примеру ниже из старого кода. Функцию эту запушить в переменную, которая при нажаити на кнопку вызывает функции запросов в цикле
+      // Добавить эот запрос в массив запросов нужно по условию, код закомментирован
+      // const { makeRequest: createFillScanProcess } = useRequest({
+      //   context,
+      //   request: () =>
+      //     store.dispatch('taskModule/loadImage', {
+      //       id: 1,
+      //       folder: 'personal_doc',
+      //       fileName: fileName,
+      //       file: form_data,
+      //     }),
+      //   successMessage: 'Файл успешно загружен',
+
+      //   $.ajax('/personal/create_fill_scan_process', {
+      //     method: "POST",
+      //     data: { parent_process: <?php echo $task['process_id']; ?>,
+      //             process_id: 1,
+      //             account_id: <?php echo $task['to_account_id']; ?>,
+      //             personal_id: <?php echo $entity['id']; ?>,
+      //             parent_action: <?php echo $task['id']; ?>,
+      //             type_parent_action: 2,
+      //             docs_id: docs_id
+      //     },
+      //     success: function() {
+      //         docsAdd = [];
+      //         slidePopup('Документы успешно прикреплены!', 'success');
+      //         checkValid();
+      //     }
+      // })
+      // })
+      let additionalRequestFlag
+      if (
+        e.item != 7 &&
+        e.item != 8 &&
+        e.item != 11 &&
+        e.item != 12 &&
+        e.item != 16 &&
+        e.item != 18 &&
+        e.item != 20 &&
+        e.item != 21
+      ) {
+        additionalRequestFlag = true
+      }
+      console.log(listDocuments.value)
+      if (!currentDropzone.inProcess) {
+        listRequestsForUpload.value.push(
+          delInfoAFile,
+          updateFileData,
+          loadImage
+        )
+        listDocuments.value[
+          listDocuments.value.findIndex((x) => x.doc_id == e.item)
+        ].inProcess = false
+        // if (additionalRequestFlag) {
+        //   listRequestsForUpload.value.push(createFillScanProcess)
+        // }
+      } else {
+        listRequestsForUpload.value.push(updateFileData, loadImage)
+        // if (additionalRequestFlag) {
+        //   listRequestsForUpload.value.push(createFillScanProcess)
+        // }
+        listDocuments.value[
+          listDocuments.value.findIndex((x) => x.doc_id == e.item)
+        ].inProcess = false
+        listDisbledDocuments.value = listDisbledDocuments.value - 1
+      }
+      console.log(listRequestsForUpload.value.length)
+    }
+
+    const sendDocuments = () => {
+      listRequestsForUpload.value.forEach((elem, index) => {
+        elem()
+      })
+      listRequestsForUpload.value = []
+    }
+
+    let sendTaskFinish = () => {
+      //   $.ajax('/common/save/personal', {
+      //     method: "POST",
+      //     data: {id: <?php echo $entity['id']; ?>, status: 5},
+      //     success: function() {
+      //     }
+      // })
+      // $.ajax('/task/change_status_task', {
+      //     method: "POST",
+      //     data: {status: 2, data: {
+      //         process_id: <?php echo $task['process_id']; ?>,
+      //         task_id: <?php echo $task['id']; ?>,
+      //         parent_action: <?php echo $task['id']; ?>
+      //     }},
+      //     success: function (data) {
+      //         slidePopup('Задача выполнена!', 'success');
+      //         typeof dataTable['task'] != "undefined" ? dataTable['task'].ajax.reload() : dataTable[document.taskTable].ajax.reload();
+      //         hideModal();
+      //     }
+      // })
+      const { makeRequest: changeStatus } = useRequest({
+        context,
+        request: () =>
+          store.dispatch('taskModule/setPartTask', {
+            status: 2,
+            data: {
+              process_id: data.task.process_id,
+              task_id: data.task.id,
+              parent_action: data.task.id,
+            },
+          }),
+      })
+
+      changeStatus()
+    }
     return {
       addFiles,
-      sendData,
       listDocuments,
-      aaaaa,
+      listRequestsForUpload,
+      sendDocuments,
+      listDisbledDocuments,
+      addFilesPatent,
+      disableFinishState,
+      sendTaskFinish,
     }
   },
 })
