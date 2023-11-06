fa6a8e52f3f95bda34e1bc43912be95395342e6b
korovkindm <korovkindm@63pokupki.ru>
2023-11-06 15:14:01 +0400

    18 задача

--- src/components/Task/form18/index.vue
+++ src/components/Task/form18/index.vue
@@ -5,14 +5,20 @@
         Назначение &nbsp;<a href="#" class="text-h6">№{{ entity.id }}</a
         >&nbsp; на дату 07.02.2023
       </v-card-title>
+      <div v-if="fileOutput">
+        <span>Скан:</span>
+        <a target="_blank" :href="fileOutput"
+          ><v-icon left small> $IconDocument </v-icon></a
+        >
+      </div>
       <TextInfo class="mb-3" :infoObj="textInfo" />
       <div>
         <v-row v-for="(group, i) in formGroup" :key="i">
-          <v-col cols="6">
+          <v-col class="p-0" cols="6">
             <v-select outlined label="Наименование"></v-select>
           </v-col>
           <v-col cols="2">
-            <v-text-field outlined label="QTY"></v-text-field>
+            <v-text-field type="number" outlined label="QTY"></v-text-field>
           </v-col>
           <v-col cols="2">
             <v-text-field
@@ -22,7 +28,7 @@
               readonly
             ></v-text-field>
           </v-col>
-          <v-col cols="2">
+          <v-col class="p-0" cols="2">
             <v-text-field
               outlined
               label="Сумма"
@@ -31,7 +37,7 @@
             ></v-text-field>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row v-if="idDirection === 6">
           <v-col style="padding: 0" cols="6">
             <v-btn
               @click="removeGroup"
@@ -51,15 +57,23 @@
             >
           </v-col>
         </v-row>
+        <v-textarea
+          v-model="formComment"
+          @input="formCommentError = ''"
+          :error-messages="formCommentError"
+          rows="2"
+          clearable
+          label="Комментарий"
+        ></v-textarea>
       </div>
     </div>
     <v-divider></v-divider>
-    <v-row class="py-2" justify="end">
-      <v-btn class="mr-2" small color="info">
+    <v-row class="pb-0 pt-3" justify="end">
+      <v-btn @click="confirmTask" class="mr-2" small color="info">
         <v-icon left> $IconMain </v-icon>
         Завершить
       </v-btn>
-      <v-btn class="mr-2" small color="error">
+      <v-btn @click="rejectTask" class="mr-2" small color="error">
         <v-icon left> $IconClose </v-icon>
         Отклонить
       </v-btn>
--- src/components/Task/form18/setup.js
+++ src/components/Task/form18/setup.js
@@ -4,9 +4,16 @@ import FormComment from '@/components/Task/el/FormComment/index.vue'
 import store from '@/store'
 import TextInfo from '@/components/Task/el/TextInfo/index.vue'
 import useForm from '@/compositions/useForm'
+import useRequest from '@/compositions/useRequest'
+import form from '@/store/modules/form'
 
 const Form18 = defineComponent({
   name: 'Form18',
+  computed: {
+    form() {
+      return form
+    },
+  },
   components: {
     TextInfo,
     FormError,
@@ -18,7 +25,7 @@ const Form18 = defineComponent({
       default: () => {},
     },
   },
-  setup(props, ctx) {
+  setup({ data }, ctx) {
     const context = {
       root: {
         store,
@@ -28,18 +35,23 @@ const Form18 = defineComponent({
     const textInfo = {
       obj: {
         key: 'Объект',
-        value: props.data.entity.object_name,
+        value: data.entity.object_name,
       },
       employee: {
         key: 'Сотрудник',
-        value: props.data.entity.personal_name,
+        value: data.entity.personal_name,
       },
       position: {
         key: 'Должность',
-        value: props.data.entity.doljnost_name,
+        value: data.entity.doljnost_name,
       },
     }
     const formGroup = ref([])
+    const fileOutput =
+      data.task.dop_data && JSON.parse(data.task.dop_data).file_output
+    const idDirection = data.entity.direction_id
+    const formCommentError = ref('')
+    const formComment = ref('')
     const addGroup = () => {
       formGroup.value = [
         ...formGroup.value,
@@ -47,7 +59,7 @@ const Form18 = defineComponent({
           fields: {
             name: {},
             qty: 0,
-            tarif: 0,
+            price: 0,
             sum: 0,
           },
           context,
@@ -56,7 +68,7 @@ const Form18 = defineComponent({
     }
     const removeGroup = () => {
       if (formGroup.value.length > 1) {
-        formGroup.value = formGroup.value.splice(-1)
+        formGroup.value = formGroup.value.slice(0, formGroup.value.length - 1)
       }
     }
 
@@ -64,12 +76,152 @@ const Form18 = defineComponent({
       addGroup()
     })
 
+    const confirmTask = async () => {
+      let total = 0
+      const services = formGroup.value.map((group, i) => {
+        const formData = group.formData
+        total += formData.sum ?? 0
+        return {
+          service_id: i,
+          qty: formData.qty,
+          price: formData.price,
+          sum: formData.sum,
+        }
+      })
+
+      const { makeRequest: setDataPayment } = useRequest({
+        context,
+        request: () => {
+          return store.dispatch('taskModule/setDataPayment', {
+            data: {
+              account_id: data.entity.manager,
+              personal_bank_id: data.entity.personal_bank_id,
+              status_id: 2,
+              status_account_id: 2,
+              personal_id: data.entity.personal_id,
+              vid_vedomost_id: 1,
+              direction_id: data.entity.direction_id,
+              object_id: data.entity.object_id,
+              bank_id: data.entity.bank_id,
+              invoice: data.entity.invoice,
+              total: total,
+              fio: data.entity.fio,
+              doljnost_id: data.entity.doljnost_id,
+              date_target: data.entity.date_target,
+              personal_target_id: data.entity.id,
+            },
+          })
+        },
+      })
+
+      const paymentData = await setDataPayment()
+
+      let targetServicesKey = null
+
+      if (data.entity.direction_id === 1) {
+        targetServicesKey =
+          [6, 9, 10, 11, 20, 21, 22, 28].indexOf(
+            Number(data.entity.doljnost_id)
+          ) === -1
+            ? 1
+            : 2
+      } else if (data.entity.direction_id === 6) {
+        targetServicesKey = 3
+      }
+
+      const targetServices = {
+        [targetServicesKey]: [
+          {
+            services: services,
+            payment_id: paymentData.result,
+            is_hold: false,
+            is_pay: false,
+            sum: total,
+          },
+        ],
+      }
+
+      const { makeRequest: setPersonalTarget } = useRequest({
+        context,
+        request: () => {
+          return store.dispatch('taskModule/setPersonalData', {
+            data: {
+              id: data.entity.id,
+              services: targetServices,
+              payment_id: paymentData.result,
+            },
+          })
+        },
+      })
+
+      const { makeRequest: changeStatusTask } = useRequest({
+        context,
+        request: () => {
+          return store.dispatch('taskModule/setPartTask', {
+            data: {
+              status: 2,
+              data: {
+                process_id: data.task.process_id,
+                manager_id: data.task.from_account_id,
+                task_id: data.task.task_id,
+                parent_action: data.task.task_id,
+                personal_target_id: data.entity.id,
+              },
+            },
+          })
+        },
+      })
+
+      await setPersonalTarget()
+      const { success } = await changeStatusTask()
+      success && ctx.emit('closePopup')
+    }
+    const rejectTask = async () => {
+      formCommentError.value = ''
+      if (confirm('Вы подтверждаете отклонение выработки?')) {
+        if (!formComment.value) {
+          formCommentError.value = 'Обязательное поле'
+          return false
+        }
+
+        const { makeRequest: changeStatusTask } = useRequest({
+          context,
+          request: () => {
+            return store.dispatch('taskModule/setPartTask', {
+              data: {
+                status: 6,
+                data: {
+                  process_id: data.task.process_id,
+                  manager_id: data.task.from_account_id,
+                  task_id: data.task.id,
+                  personal_target_id: data.entity.id,
+                  parent_action: data.task.id,
+                  comment: formComment,
+                  error: 1,
+                  need_input: 1,
+                  need_parse: 0,
+                },
+              },
+            })
+          },
+        })
+        const { success } = await changeStatusTask()
+        success && ctx.emit('closePopup')
+      }
+    }
+
     return {
       textInfo,
       addGroup,
       removeGroup,
       formGroup,
-      entity: props.data.entity,
+      entity: data.entity,
+      fileOutput,
+      idDirection,
+      confirmTask,
+      rejectTask,
+      formComment,
+      formCommentError,
     }
   },
 })
