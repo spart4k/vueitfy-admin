b34157ec030a7af3128533c95997d332e65f787d
korovkindm <korovkindm@63pokupki.ru>
2023-12-13 19:28:14 +0400

    fix

--- src/components/Task/form20/setup.js
+++ src/components/Task/form20/setup.js
@@ -58,7 +58,7 @@ const Form20 = defineComponent({
         return store.dispatch('taskModule/setPartTask', {
           status: 2,
           data: {
-            process_id: props.data.entity.process_id,
+            process_id: props.data.task.process_id,
             task_id: props.data.entity.id,
             parent_action: props.data.entity.id,
             user_key: props.data.entity.id,
