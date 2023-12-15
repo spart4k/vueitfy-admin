c618cf40cf7cc6a69b12d6f532b8f745304e297f
SotnikovMaksim <1007@tavr.su>
2023-12-15 13:24:16 +0300

    init: fixing double query

--- src/components/Table/default/setup.js
+++ src/components/Table/default/setup.js
@@ -277,19 +277,12 @@ const table = {
       filter.value.isShow = false
     }
     const getItems = async () => {
-      console.log('get items')
-      //this.
       loading.value = true
       const { url } = props.options.options
-      //const data = await tableApi.get(url, {
-      //  currentPage: pagination.value.currentPage,
-      //  countRows: 30,
-      //  searchGlobal: searchField.value,
-      //})
-      //body.sorts = Object.assign(target, source).sorts
+
       let sorts = []
       let searchColumns = []
-      //let filter = []
+
       paramsQuery.value.sorts.forEach((el) => {
         if (!el.value) {
           return
@@ -304,19 +297,7 @@ const table = {
           searchColumns.push(el)
         }
       })
-      //props.filtersConfig.forEach((el) => {
-      //  if (!el.value) {
-      //    return
-      //  } else {
-      //    filter.push({
-      //      field: el.name,
-      //      value: el.value,
-      //      alias: el.alias,
-      //      type: el.type,
-      //      subtype: el.subtype,
-      //    })
-      //  }
-      //})
+
       let by = undefined
       // console.log('props.filtersConfig', store.state.formStorage, props.detail?.stageData.id)
       if (props.routeParam || store?.state?.formStorage?.id) {
@@ -329,7 +310,6 @@ const table = {
           },
         ]
       }
-      console.log('table get')
       const data = await store.dispatch('table/get', {
         url: url,
         data: {
@@ -506,19 +486,39 @@ const table = {
       () => {
       }
     )
-    //watch(
-    //  () => pagination.value.currentPage,
-    //  async () => {
-    //    await getItems()
-    //  }
-    //)
-    watch(
-      () => paramsQuery,
-      async () => {
-        await getItems()
-      },
-      { deep: true }
-    )
+
+
+    // const paramsQuery = ref({
+    //   currentPage: pagination.value.currentPage,
+    //   searchGlobal: searchField.value,
+    //   countRows: pagination.value.countRows,
+    //   sorts: [],
+    //   searchColumns: [],
+    // })
+
+    
+      watch(
+        () => paramsQuery,
+        async () => {
+          await getItems()
+        },
+        {deep: true}
+      )
+
+      watch(
+        () => paramsQuery.value.sorts,
+        async () => {
+          await getItems()
+        },
+      )
+
+      watch(
+        () => paramsQuery.value.searchColumns ,
+        async () => {
+          await getItems()
+        },
+      )
+    
 
     // HOOKS
     onMounted(async () => {
