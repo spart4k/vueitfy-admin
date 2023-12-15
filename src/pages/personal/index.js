b579fe5f5b19c99f456c677913cf97b011f187ea
Dmitrij <tolstov.dmitrij42@gmail.com>
2023-12-15 16:15:21 +0400

    fix: personal add btn color + request

--- src/pages/personal/index.js
+++ src/pages/personal/index.js
@@ -732,6 +732,22 @@ const documentConfig = {
             },
             value: '',
           }),
+          checkboxField({
+            label: '',
+            name: 'from_document_prishel',
+            value: true,
+            placeholder: '',
+            readonly: false,
+            class: [''],
+            position: {
+              cols: 12,
+              sm: 12,
+            },
+            isShow: {
+              value: true,
+            },
+            bootstrapClass: [''],
+          }),
           textareaField({
             label: 'Примечание:',
             name: 'note',
@@ -816,6 +832,22 @@ const documentConfig = {
               paramsForEmit: this,
             },
             value: '',
+          }),
+          checkboxField({
+            label: '',
+            name: 'from_document_prishel',
+            value: true,
+            placeholder: '',
+            readonly: false,
+            class: [''],
+            position: {
+              cols: 12,
+              sm: 12,
+            },
+            isShow: {
+              value: true,
+            },
+            bootstrapClass: [''],
           }),
           textareaField({
             label: 'Примечание:',
@@ -2561,7 +2593,7 @@ const defaultForm = [
           stringAction({
             text: 'Вернуться',
             type: 'submit',
-            color: 'black',
+            color: 'disabled',
             module: 'form/del',
             url: 'delete/unfinished_personal',
             name: 'prevStage',
