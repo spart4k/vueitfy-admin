b38ec424cd44f5caff6a77c56ed21f78bc7ed446
Aleksey Tihonravov <fosterfosterfoster63@gmail.com>
2023-12-18 15:57:55 +0400

    add btn refresh

--- src/pages/object/index.js
+++ src/pages/object/index.js
@@ -7,6 +7,7 @@ import {
   dateField,
   checkboxField,
   colorPicker,
+  textBlock,
   // dropZoneField,label:"label:"ФИО директора"
 } from '@/utils/fields.js'
 import { stringAction } from '@/utils/actions'
@@ -3016,6 +3017,12 @@ const config = {
                     value: '',
                   },
                 ],
+                dependence: [
+                  {
+                    type: 'default',
+                    fillField: ['permission_id'],
+                  },
+                ],
                 // filters: [
                 //   {
                 //     field: 'direction_id',
@@ -3035,6 +3042,20 @@ const config = {
                 //   fields: ['personal_id'],
                 // },
               }),
+              textBlock({
+                label: 'Создал',
+                name: 'permission_id',
+                placeholder: '',
+                readonly: true,
+                class: [''],
+                position: {
+                  cols: 12,
+                  sm: 12,
+                },
+                bootstrapClass: [''],
+                //validations: { required },
+                //isShow: false,
+              }),
             ],
             actions: [
               stringAction({
@@ -3050,9 +3071,9 @@ const config = {
                 text: 'Сохранить',
                 type: 'submit',
                 module: 'form/create',
-                name: 'saveForm',
-                url: 'create/assignt',
-                action: 'saveForm',
+                name: 'createForm',
+                url: 'create/assign',
+                action: 'createForm',
                 color: 'primary',
               }),
             ],
