164c5ac0cbb11280b41157ccafb517ed2b5b27e7
Tair <tairenikeev@gmail.com>
2024-02-13 12:00:58 +0400

    completely fixed filter

--- src/views/PersonalView.vue
+++ src/views/PersonalView.vue
@@ -15,7 +15,7 @@
     </v-tabs>
     <!-- TODO: Компоненты ФОРМЫ -->
     <v-tabs-items v-model="activeTab">
-      <v-tab-item v-for="item in config.tabs" :key="item.options">
+      <v-tab-item v-for="item in config.tabs" :key="item.options.title">
         <component
           :is="item.type"
           @changeheadershow="changeheadershow"
