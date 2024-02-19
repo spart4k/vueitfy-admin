fe9df5bdeda6b341a3678b351fa696fba0956e57
Aleksey Tikhonravov <fosterfosterfoster63@gmail.com>
2024-02-19 17:27:12 +0400

    fix alias table

--- src/pages/personal/index.js
+++ src/pages/personal/index.js
@@ -1685,7 +1685,7 @@ const bankConfig = {
           isShow: false,
         },
       ],
-      alias: 'ab.id',
+      alias: 'pb.id',
       isShow: true,
       width: '40',
       value: 'id',
@@ -1713,7 +1713,7 @@ const bankConfig = {
       isShow: true,
       width: '150',
       value: 'priority',
-      alias: 'ab.priority',
+      alias: 'pb.priority',
       search: {
         field: '',
         isShow: true,
@@ -1762,7 +1762,7 @@ const bankConfig = {
       ],
       isShow: true,
       width: '150',
-      alias: 'ab.invoice',
+      alias: 'pb.invoice',
       value: 'invoice',
       search: {
         field: '',
@@ -1788,7 +1788,7 @@ const bankConfig = {
       isShow: true,
       width: '150',
       value: 'fio',
-      alias: 'ab.fio',
+      alias: 'pb.fio',
       search: {
         field: '',
         isShow: true,
