419d59b528a17fc787a78b6d6b40a88ee645ce12
Tair <tairenikeev@gmail.com>
2024-02-20 15:16:11 +0400

    updated form11

--- src/components/Task/form11/index.vue
+++ src/components/Task/form11/index.vue
@@ -1,37 +1,64 @@
 <template>
   <div>
     <div style="padding-top: 20px">
-      <span class="font-weight-bold text-h6"
+      <span class="font-weight-bold heading"
         >Проверьте закрывающие документы:</span
       >
       <div class="position-relative mb-8">
         <!-- TODO: LIST -->
-        <div>
-          <v-list lines="one" class="overflow-y-auto" max-height="180">
-            <v-list-item v-for="file in 3" :key="file" class="file-item mb-3">
+        <div v-if="listRequestsForUpload?.length">
+          <div class="alert" v-if="errors.isActive">
+            <span>{{ errors.message }}</span>
+          </div>
+          <v-list lines="one" class="list overflow-y-auto" max-height="200">
+            <v-list-item
+              v-for="(file, fileID) in listRequestsForUpload"
+              :key="file"
+              class="file-item mb-3"
+            >
               <div class="file-img">
-                <v-img src="../../../assets/image/doc.png"></v-img>
+                <v-img class="img" :src="file.dataURL"></v-img>
               </div>
-              <div class="file-name">Item {{ file * 1000 }}</div>
-              <div class="file-remove" @click="removeFile">
+              <div class="file-name">{{ file.name }}</div>
+              <div class="file-remove" @click="removeFile(fileID)">
                 <IconDelete />
               </div>
             </v-list-item>
           </v-list>
         </div>
+
+        <div v-if="!listRequestsForUpload?.length" class="text-center mt-4">
+          <span class="font-weight-regular text-subtitle-1"
+            >Документы не загружены</span
+          >
+        </div>
       </div>
       <!-- TODO: INPUT -->
       <v-row class="pb-2 px-0" justify="center">
         <v-col class="ps-0" align-self="center">
-          <span class="font-weight-bold text-h6"
+          <span class="font-weight-bold heading"
             >Приложите закрывающие документы:</span
           >
-          <DropZone @addFiles="addFiles" :options="dropzoneOptions"></DropZone>
+          <DropZone
+            ref="dropZone"
+            @addFiles="addFiles"
+            :options="dropzoneOptions"
+          ></DropZone>
           <!-- <DropZone
             :paramsForEmit="{ item: item.doc_id }"
             @addFiles="addFiles"
             :options="dropzoneOptions"
           ></DropZone> -->
+          <div class="d-flex justify-end">
+            <v-btn
+              @click="sendDocuments"
+              small
+              variant="tonal"
+              color="orange"
+              :disabled="!isDocs"
+              >Приложить</v-btn
+            >
+          </div>
         </v-col>
       </v-row>
       <!-- TODO: Комментарии -->
@@ -53,11 +80,21 @@
           <v-icon small>mdi-close</v-icon>
           Закрыть
         </v-btn>
+        <!-- <v-btn
+          color="info"
+          @click="sendTaskFinish"
+          small
+          :disabled="(!comment.length && refds) || !disabledDocumentsAcc"
+        >
+          <v-icon small>mdi-content-save</v-icon>
+          Завершить
+        </v-btn> -->
+        <!-- FIXME: починить disabled -->
         <v-btn
           color="info"
           @click="sendTaskFinish"
           small
-          :disabled="!refds || (!disabledDocumentsAcc && !comment)"
+          :disabled="comment && !refds"
         >
           <v-icon small>mdi-content-save</v-icon>
           Завершить
@@ -68,4 +105,4 @@
 </template>
 
 <script src="./setup.js"></script>
-<style lang="scss" src="./index.scss"></style>
+<style lang="scss" src="./index.scss" scoped></style>
--- src/components/Task/form11/setup.js
+++ src/components/Task/form11/setup.js
@@ -1,4 +1,11 @@
-import { defineComponent, ref, computed, onMounted } from 'vue'
+import {
+  defineComponent,
+  ref,
+  computed,
+  onMounted,
+  onUpdated,
+  reactive,
+} from 'vue'
 import useForm from '@/compositions/useForm'
 import { required } from '@/utils/validation'
 import useRequest from '@/compositions/useRequest'
@@ -6,6 +13,7 @@ import store from '@/store'
 import { useRouter, useRoute } from 'vue-router/composables'
 import DropZone from '@/components/Dropzone/default/index.vue'
 import IconDelete from '@/components/Icons/delete/delete.vue'
+import { methods } from 'vue2-dropzone'
 
 const Form11 = defineComponent({
   name: 'Form11',
@@ -13,7 +21,6 @@ const Form11 = defineComponent({
     DropZone,
     IconDelete,
   },
-
   props: {
     data: {
       type: Object,
@@ -23,14 +30,24 @@ const Form11 = defineComponent({
   setup({ data }, ctx) {
     const route = useRoute()
     const router = useRouter()
-    const files = ref(null)
 
+    const context = {
+      root: {
+        store,
+        router,
+        ctx,
+        route,
+      },
+    }
     const dropzoneOptions = ref({
+      withoutSave: false,
+      folder: 'tmp',
       //url: 'https://httpbin.org/post',
       //url: '/save/:folder/:file',
       url: 'http://10.63.1.132:5000/file/save/act/TESTFILE.jpg',
       autoProcessQueue: false,
       autoDiscover: false,
+      acceptedFiles: 'image/*',
       thumbnailWidth: 150,
       // maxFilesize: props.options.maxSize ? props.options.maxSize : 10,
       // maxFiles: props.options.countFiles ? props.options.countFiles : 1,
@@ -44,83 +61,85 @@ const Form11 = defineComponent({
       // headers: { "My-Awesome-Header": "header value" }
     })
 
-    const context = {
-      root: {
-        store,
-        router,
-        ctx,
-        route,
-      },
-    }
-
+    //Variables
     const account_id = computed(() => store.state.user.account_id)
     const chied_id = computed(() => store.state.user.chied_id)
     let listDocuments = ref([])
     let listDisbledDocuments = ref(0)
     let sss = JSON.parse(data.task.dop_data)
+    let file = ref(null)
     let comment = ref('')
+    let listRequestsForUpload = ref([])
+    let docs_ids = ref([])
+    let disabledDocumentsAcc = ref(0)
+    let refds = ref(0)
+    let errors = ref({
+      isActive: false,
+      message: 'Ошибка',
+    })
+    let isDocs = ref(false)
+    let dropZone = ref(null)
 
     onMounted(() => {
-      sss.docs_id.forEach((item) => {
-        let pasteObject = data.data.docs.find((doc) => doc.doc_id === item)
-        if (pasteObject) {
-          pasteObject['inProcess'] = true
-        } else {
-          pasteObject = { doc_id: item, inProcess: false }
-        }
-        listDocuments.value.push(pasteObject)
-      })
+      // sss.docs_id.forEach((item) => {
+      //   let pasteObject = data.data.docs.find((doc) => doc.doc_id === item)
+      //   if (pasteObject) {
+      //     pasteObject['inProcess'] = true
+      //   } else {
+      //     pasteObject = { doc_id: item, inProcess: false }
+      //   }
+      //   listDocuments.value.push(pasteObject)
+      // })
     })
 
-    let listRequestsForUpload = ref([])
+    // let addFilesPatent = (e, options) => {
+    //   let fileExt = e[0].type.split('/')[1]
+    //   let fileName = `personal_doc_` + Date.now() + '.' + fileExt
+    //   let form_data = new FormData()
+    //   form_data.append('file', e[0])
 
-    let docs_ids = ref([])
-    let addFilesPatent = (e, options) => {
-      let fileExt = e[0].type.split('/')[1]
-      let fileName = `personal_doc_` + Date.now() + '.' + fileExt
-      let form_data = new FormData()
-      form_data.append('file', e[0])
+    //   const { makeRequest: updateFileData } = useRequest({
+    //     context,
+    //     request: () =>
+    //       store.dispatch('taskModule/updateFileData', {
+    //         personal_id: data.entity.id,
+    //         doc_id: e.item,
+    //         path_doc: `/personal_doc/${fileName}`,
+    //         from_task: true,
+    //       }),
+    //   })
 
-      const { makeRequest: updateFileData } = useRequest({
-        context,
-        request: () =>
-          store.dispatch('taskModule/updateFileData', {
-            personal_id: data.entity.id,
-            doc_id: e.item,
-            path_doc: `/personal_doc/${fileName}`,
-            from_task: true,
-          }),
-      })
+    //   const { makeRequest: loadImage } = useRequest({
+    //     context,
+    //     request: () =>
+    //       store.dispatch('taskModule/loadImage', {
+    //         id: 1,
+    //         folder: 'personal_doc',
+    //         fileName: fileName,
+    //         file: form_data,
+    //       }),
+    //     successMessage: 'Файл успешно загружен',
+    //   })
+    //   updateFileData()
+    //   loadImage()
+    // }
 
-      const { makeRequest: loadImage } = useRequest({
-        context,
-        request: () =>
-          store.dispatch('taskModule/loadImage', {
-            id: 1,
-            folder: 'personal_doc',
-            fileName: fileName,
-            file: form_data,
-          }),
-        successMessage: 'Файл успешно загружен',
-      })
-      updateFileData()
-      loadImage()
-    }
-    let refds = ref(0)
     let addFiles = (e, options) => {
-      console.log(e, options, listDocuments.value)
-      let fileExt = e[0].type.split('/')[1]
+      file.value = e[0]
+      let fileExt = file.value.type.split('/')[1]
       let fileName = `personal_doc_` + Date.now() + '.' + fileExt
       let form_data = new FormData()
-      form_data.append('file', e[0])
-      refds.value + 1
+      form_data.append('file', file.value)
+      refds.value = 0
       let currentDropzone = listDocuments.value.find((x) => x.doc_id === e.item)
       docs_ids.value.push(e.item)
+
       const { makeRequest: delInfoAFile } = useRequest({
         context,
         request: () =>
           store.dispatch('taskModule/updateFileData', { id: e.item, del: 1 }),
       })
+
       const { makeRequest: updateFileData } = useRequest({
         context,
         request: () =>
@@ -143,42 +162,71 @@ const Form11 = defineComponent({
           }),
         successMessage: 'Файл успешно загружен',
       })
-      if (currentDropzone.inProcess) {
-        listRequestsForUpload.value.push(
-          delInfoAFile,
-          updateFileData,
-          loadImage
-        )
-        listDocuments.value[
-          listDocuments.value.findIndex((x) => x.doc_id == e.item)
-        ].inProcess = false
-      } else {
-        listRequestsForUpload.value.push(updateFileData, loadImage)
-        listDocuments.value[
-          listDocuments.value.findIndex((x) => x.doc_id == e.item)
-        ].inProcess = false
-      }
-      console.log(listRequestsForUpload.value.length)
-      console.log(listDocuments.value)
+
+      listDocuments.value.push(e[0])
+
+      // if (currentDropzone.inProcess) {
+      //   listRequestsForUpload.value.push(
+      //     delInfoAFile,
+      //     updateFileData,
+      //     loadImage
+      //   )
+      //   listDocuments.value[
+      //     listDocuments.value.findIndex((x) => x.doc_id == e.item)
+      //   ].inProcess = false
+      // } else {
+      //   listRequestsForUpload.value.push(updateFileData, loadImage)
+      //   listDocuments.value[
+      //     listDocuments.value.findIndex((x) => x.doc_id == e.item)
+      //   ].inProcess = false
+      // }
+
+      isDocs.value = 1
+      file.value = null
     }
 
-    const removeFile = () => {
-      // Удалено
+    const removeFile = (fileID) => {
+      console.log('REMOVE', fileID)
+
+      listRequestsForUpload.value = listRequestsForUpload.value.filter(
+        (item, ID) => ID !== fileID
+      )
+      listDocuments.value = listDocuments.value.filter(
+        (item, ID) => ID !== fileID
+      )
+
+      if (listRequestsForUpload.value.length == 0) {
+        refds.value = 0
+      }
     }
 
     const sendDocuments = () => {
-      listRequestsForUpload.value.forEach((elem, index) => {
-        elem()
-      })
+      // listRequestsForUpload.value.forEach((elem, index) => {
+      //   elem()
+      // })
+      let newFile = listDocuments.value[listDocuments.value.length - 1]
+      listRequestsForUpload.value = listDocuments.value
+      refds.value = 1
+      isDocs.value = 0
+
+      // console.log(listDocuments.value[0])
+      // console.log(listRequestsForUpload.value)
+
+      console.log('NEWFIle', newFile)
+
+      if (
+        listRequestsForUpload.value.every((file) => file.name !== newFile.name)
+      )
+        console.log('Файлы совпадают')
+
+      dropZone.value.clearDropzone()
     }
-    let disabledDocumentsAcc = ref(0)
+
     let addDisabledDocuments = (elem) => {
-      console.log(listDocuments.value)
       let keyOfObject = listDocuments.value.findIndex(
         (x) => x.doc_id == elem.item
       )
       listDocuments.value[keyOfObject].inProcess = true
-      console.log(listDocuments.value)
       disabledDocumentsAcc.value + 1
     }
 
@@ -251,21 +299,24 @@ const Form11 = defineComponent({
     }
 
     return {
+      file,
+      isDocs,
       addFiles,
       removeFile,
       listDocuments,
-      // listRequestsForUpload,
-      // sendDocuments,
+      listRequestsForUpload,
+      sendDocuments,
       listDisbledDocuments,
       // addFilesPatent,
       comment,
-      files,
+      dropZone,
       dropzoneOptions,
       // sendTaskFinish,
-      // addDisabledDocuments,
-      // disabledDocumentsAcc,
-      // emplyeeFired,
-      // refds,
+      addDisabledDocuments,
+      disabledDocumentsAcc,
+      emplyeeFired,
+      errors,
+      refds,
     }
   },
 })
