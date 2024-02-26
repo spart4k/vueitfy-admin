dd5b27d19785f40e347f669df1c78d748afaf89d
Aleksey Tikhonravov <fosterfosterfoster63@gmail.com>
2024-02-26 11:09:42 +0400

    transform logic to view-component

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -1,7 +1,10 @@
 <template>
   <!--<Layout>-->
   <div class="d-flex flex-column flex-grow-1 h-100">
-    <TableDefault @changeheadershow="changeheadershow" :options="payment" />
+    <TableDefault
+      @changeheadershow="changeheadershow"
+      :options="paymentConfig"
+    />
   </div>
   <!--</Layout>-->
 </template>
@@ -10,6 +13,9 @@
 import { payment } from '@/pages'
 
 import TableDefault from '@/components/Table/default/index.vue'
+import { defaultForm as personalConfig } from '@/pages/personal/index'
+import _ from 'lodash'
+import { onMounted, ref } from 'vue'
 
 export default {
   name: 'Payment-View',
@@ -26,8 +32,36 @@ export default {
   },
   async mounted() {},
   setup() {
+    let paymentConfig = {}
+    const loading = ref(true)
+    const initConfig = () => {
+      // const changeActionTo = (array, key, oldPath, newPath) => {
+      //   console.log('changeActionTo')
+      //   array.forEach((tab) => {
+      //     if (tab.path === oldPath) {
+      //       tab.path = newPath
+      //     }
+      //     if (tab.actions) {
+      //       tab.actions.forEach((el) => {
+      //         if (el.action === 'closePopup') {
+      //           el.to = key
+      //         }
+      //       })
+      //     }
+      //   })
+      // }
+      // const personalConfigForms = _.cloneDeep(personalConfig)
+      paymentConfig = _.cloneDeep(payment)
+      // changeActionTo(personalConfigForms, 'pivot', 'edit', 'edit-personal')
+      // paymentConfig.detail.tabs.push(personalConfigForms)
+      loading.value = false
+    }
+    onMounted(() => {
+      initConfig()
+    })
     return {
-      payment,
+      // payment,
+      paymentConfig,
     }
   },
 }
