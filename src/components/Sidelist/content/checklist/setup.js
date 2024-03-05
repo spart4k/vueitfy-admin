e38215b380ac960bbaa9d36c96c17618664087ab
Dmitrij <tolstov.dmitrij42@gmail.com>
2024-03-05 16:00:26 +0400

    add: checklist content request

--- src/components/Sidelist/content/checklist/setup.js
+++ src/components/Sidelist/content/checklist/setup.js
@@ -13,13 +13,20 @@ export default {
   setup(props) {
     const stage = ref(0)
     const loading = ref(true)
-    const data = ref({})
+    const data = ref({
+      content: {},
+      period: {},
+    })
 
     const getData = async () => {
       loading.value = true
-      data.value = await store.dispatch(
+      data.value.period = await store.dispatch(
         'table/getDetail',
         `get/period/month?period=${props.date.date}`
+      )
+      data.value.content = await store.dispatch(
+        'table/getDetail',
+        `get/checklist/${data.value.period.data.id}`
       )
       loading.value = false
     }
