9ede122807e5a574519480663529a9602a973659
Aleksey Tikhonravov <fosterfosterfoster63@gmail.com>
2024-03-11 14:04:44 +0400

    add mock row

--- src/components/PaymentList/row/index.vue
+++ src/components/PaymentList/row/index.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="personal">
     <div class="personal-wrap">
-      <div class="personal-head">
-        <div class="personal-head-info"></div>
-        <div class="personal-head-panel"></div>
-      </div>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="personal-head">
+              <div class="personal-head-info">
+                <v-checkbox class=""></v-checkbox>
+                <span>Петя Петин</span>
+              </div>
+              <div class="personal-head-panel"></div>
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content> asdasd </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
   </div>
 </template>
+<style lang="scss" src="./style.scss"></style>
+<script src="./setup.js"></script>
