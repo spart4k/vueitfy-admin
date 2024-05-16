71425092ef2eae01205f6c37f675a7651cc3288d
Alexander Nefedov <alexandernefedov@MacBook-Air-Alexander.local>
2024-05-16 10:08:16 +0400

    fix: remove validation

--- src/pages/personal/config/form-personal-edit.js
+++ src/pages/personal/config/form-personal-edit.js
@@ -420,7 +420,7 @@ export default {
         cols: 12,
         sm: 6,
       },
-      validations: { required },
+      // validations: { required },
       bootstrapClass: [''],
       filter: [
         {
