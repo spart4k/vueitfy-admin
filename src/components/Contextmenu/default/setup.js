f21e41589b1031ff2f72efc8a7f8d74ba602da89
Alexander Nefedov <alexandernefedov@MacBook-Air-Alexander.local>
2024-05-27 11:00:30 +0400

    fix: personal context

--- src/components/Contextmenu/default/setup.js
+++ src/components/Contextmenu/default/setup.js
@@ -81,7 +81,7 @@ export default {
         return result
       }
       const checkIncludesPermissions = (el) => {
-        return el.permission_id.includes(permission.value)
+        return el.permissions.includes(permission.value)
       }
       const checkIncludesVertical = (el) => {
         return el.is_personal_vertical.includes(is_personal_vertical.value)
@@ -97,7 +97,7 @@ export default {
               ) {
                 return checkIncludesData(conditionEl) && conditionEl.type
               } else if (
-                conditionEl.permission_id?.length &&
+                conditionEl.permissions?.length &&
                 !conditionEl.target
               ) {
                 return checkIncludesPermissions(conditionEl) && conditionEl.type
