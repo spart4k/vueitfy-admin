34a65e5c3230e24fd44161dfb9996aa4c8578e6c
Alexander Nefedov <alexandernefedov@MacBook-Air-Alexander.local>
2024-06-05 13:35:19 +0400

    fix: updatelist(ya dolbaeb)

--- src/pages/zayavka/index.js
+++ src/pages/zayavka/index.js
@@ -4477,78 +4477,78 @@ const config = {
           ],
         },
       },
-      {
-        label: 'Согласовать',
-        class: ['v-table-button--custom'],
-        backgroundColor: '#fff',
-        type: 'selectedItems',
-        refreshTable: true,
-        method: async (context) => {
-          const data = await context.store.dispatch('form/putForm', {
-            url: 'mass/zayavka/agree',
-            body: { data: { ids: context.idArray } },
-          })
-          if (data.code === 1) {
-            context.store.commit('notifies/showMessage', {
-              color: 'success',
-              content: 'Заявки согласованы',
-              timeout: 2000,
-            })
-          } else if (data.code === 2) {
-            context.store.commit('notifies/showMessage', {
-              color: 'warning',
-              content: 'Не выбрано ни одной записи',
-              timeout: 2000,
-            })
-          } else if (data.code === 3) {
-            context.store.commit('notifies/showMessage', {
-              color: 'error',
-              content: 'Что-то пошло не так...',
-              timeout: 2000,
-            })
-          }
-        },
-      },
-      {
-        label: 'Оплачено',
-        class: ['v-table-button--custom'],
-        backgroundColor: '#fff',
-        type: 'selectedItems',
-        refreshTable: true,
-        isShow: {
-          condition: [
-            {
-              permissions: [4, 12, 22],
-              type: true,
-            },
-          ],
-        },
-        method: async (context) => {
-          const data = await context.store.dispatch('form/putForm', {
-            url: 'mass/zayavka/pay',
-            body: { data: { ids: context.idArray } },
-          })
-          if (data.code === 1) {
-            context.store.commit('notifies/showMessage', {
-              color: 'success',
-              content: 'Заявки оплачены',
-              timeout: 2000,
-            })
-          } else if (data.code === 2) {
-            context.store.commit('notifies/showMessage', {
-              color: 'warning',
-              content: 'Не выбрано ни одной записи',
-              timeout: 2000,
-            })
-          } else if (data.code === 3) {
-            context.store.commit('notifies/showMessage', {
-              color: 'error',
-              content: 'Что-то пошло не так...',
-              timeout: 2000,
-            })
-          }
-        },
-      },
+      // {
+      //   label: 'Согласовать',
+      //   class: ['v-table-button--custom'],
+      //   backgroundColor: '#fff',
+      //   type: 'selectedItems',
+      //   refreshTable: true,
+      //   method: async (context) => {
+      //     const data = await context.store.dispatch('form/putForm', {
+      //       url: 'mass/zayavka/agree',
+      //       body: { data: { ids: context.idArray } },
+      //     })
+      //     if (data.code === 1) {
+      //       context.store.commit('notifies/showMessage', {
+      //         color: 'success',
+      //         content: 'Заявки согласованы',
+      //         timeout: 2000,
+      //       })
+      //     } else if (data.code === 2) {
+      //       context.store.commit('notifies/showMessage', {
+      //         color: 'warning',
+      //         content: 'Не выбрано ни одной записи',
+      //         timeout: 2000,
+      //       })
+      //     } else if (data.code === 3) {
+      //       context.store.commit('notifies/showMessage', {
+      //         color: 'error',
+      //         content: 'Что-то пошло не так...',
+      //         timeout: 2000,
+      //       })
+      //     }
+      //   },
+      // },
+      // {
+      //   label: 'Оплачено',
+      //   class: ['v-table-button--custom'],
+      //   backgroundColor: '#fff',
+      //   type: 'selectedItems',
+      //   refreshTable: true,
+      //   isShow: {
+      //     condition: [
+      //       {
+      //         permissions: [4, 12, 22],
+      //         type: true,
+      //       },
+      //     ],
+      //   },
+      //   method: async (context) => {
+      //     const data = await context.store.dispatch('form/putForm', {
+      //       url: 'mass/zayavka/pay',
+      //       body: { data: { ids: context.idArray } },
+      //     })
+      //     if (data.code === 1) {
+      //       context.store.commit('notifies/showMessage', {
+      //         color: 'success',
+      //         content: 'Заявки оплачены',
+      //         timeout: 2000,
+      //       })
+      //     } else if (data.code === 2) {
+      //       context.store.commit('notifies/showMessage', {
+      //         color: 'warning',
+      //         content: 'Не выбрано ни одной записи',
+      //         timeout: 2000,
+      //       })
+      //     } else if (data.code === 3) {
+      //       context.store.commit('notifies/showMessage', {
+      //         color: 'error',
+      //         content: 'Что-то пошло не так...',
+      //         timeout: 2000,
+      //       })
+      //     }
+      //   },
+      // },
       // {
       //   label: 'Скачать',
       //   class: ['v-table-button--custom'],
