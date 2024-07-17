684da23d19f1ae9a9cf0501a8b174b1a3649badb
Alexander Nefedov <alexandernefedov@MacBook-Air-Alexander.local>
2024-07-17 17:40:24 +0400

    fix: handler

--- src/pages/personal/config/table-personal-x5.js
+++ src/pages/personal/config/table-personal-x5.js
@@ -218,7 +218,7 @@ export default {
                 },
                 3: {
                   text: 'Ключ с таким номером уже существует',
-                  color: 'error',
+                  color: 'warning',
                 },
               },
             }),
@@ -319,7 +319,7 @@ export default {
                 },
                 3: {
                   text: 'Ключ с таким номером уже существует',
-                  color: 'error',
+                  color: 'warning',
                 },
               },
             }),
