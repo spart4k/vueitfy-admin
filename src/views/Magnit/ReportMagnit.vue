51e0056f526c7502a465abda77794e00ae32b47b
Alexander Nefedov <alexandernefedov@MacBook-Air-Alexander.local>
2024-08-21 10:14:43 +0400

    add: report_magnit & report_filial

---
+++ src/views/Magnit/ReportMagnit.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--<Layout>-->
+  <div class="d-flex flex-column flex-grow-1 h-100">
+    <TableDefault
+      @changeheadershow="changeheadershow"
+      :options="reportMagnit"
+    />
+  </div>
+  <!--</Layout>-->
+</template>
+
+<script>
+import { reportMagnit } from '@/pages'
+
+//import Layout from '@/layouts/default/index'
+//import Axios from 'axios'
+
+export default {
+  name: 'Report-Magnit-View',
+  components: {
+    //Layout,
+  },
+  methods: {
+    changeheadershow(options) {
+      const { headerEl, value } = options
+      headerEl.isShow = value
+    },
+  },
+  setup() {
+    return {
+      reportMagnit,
+    }
+  },
+}
+</script>
