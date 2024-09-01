d6372a42ed61444c5cd78e04b9f77044e78b1452
D3phyr <sanya.dadaya@mail.ru>
2024-09-01 21:29:30 +0400

    add: personal report 2/3

--- src/pages/personal/config/custom-personal-report/index.vue
+++ src/pages/personal/config/custom-personal-report/index.vue
@@ -1,11 +1,16 @@
 <template>
   <v-card class="pt-3 pb-6 px-6">
     <v-row class="d-flex justify-space-between">
-      <v-btn class="px-0" width="110" color="textGray" text>
-        <v-icon color="gray">mdi-chevron-left</v-icon> {{ getMonth(date, -1) }}
+      <v-btn
+        @click="changeMonth(-1)"
+        class="px-0"
+        width="110"
+        color="textGray"
+        text
+      >
+        <v-icon color="gray">mdi-chevron-left</v-icon> {{ getMonth(-1) }}
       </v-btn>
       <v-menu
-        v-model="menu"
         :close-on-content-click="false"
         transition="scale-transition"
         offset-y
@@ -20,7 +25,7 @@
             color="text"
             text
             ><v-icon color="gray" class="mr-3">mdi-calendar-text-outline</v-icon
-            >{{ getMonth(date) }}, {{ getYear(date) }}
+            >{{ getMonth() }}, {{ getYear(date) }}
           </v-btn>
         </template>
         <v-date-picker
@@ -31,15 +36,120 @@
           :first-day-of-week="1"
           reactive
         >
-          <!-- <v-spacer></v-spacer> -->
-          <!-- <v-btn text color="primary" @click="menu = false"> OK </v-btn> -->
         </v-date-picker>
       </v-menu>
-      <v-btn class="px-0" width="110" color="textGray" text
-        >{{ getMonth(date, 1) }} <v-icon color="gray">mdi-chevron-right</v-icon>
+      <v-btn
+        @click="changeMonth(1)"
+        class="px-0"
+        width="110"
+        color="textGray"
+        text
+        >{{ getMonth(1) }} <v-icon color="gray">mdi-chevron-right</v-icon>
       </v-btn>
     </v-row>
     <v-divider class="mt-3"></v-divider>
+    <div class="py-3 overflow-auto" style="height: 400px">
+      <div
+        class="d-flex justify-center align-center"
+        style="height: 100%"
+        v-if="loading.period"
+      >
+        <v-progress-circular color="primary" :size="80" indeterminate />
+      </div>
+      <template v-else>
+        <template v-if="data.period.length">
+          <v-row v-for="item in data.period" :key="item.parser_id">
+            <v-col cols="12" sm="4" class="px-0 d-flex"
+              ><v-icon
+                size="24"
+                class="mr-6 cursor-default"
+                :color="item.success ? 'success' : 'error'"
+                >{{
+                  item.success
+                    ? 'mdi-checkbox-marked-circle'
+                    : 'mdi-close-circle'
+                }}</v-icon
+              >
+              <div>
+                <div class="text--text font-weight-500">
+                  Автовыгрузка №{{ item.parser_id }}
+                </div>
+                <div class="text--disabled font-weight-500">Июнь 2024</div>
+              </div>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="4"
+              class="px-6 d-flex align-center justify-center"
+            >
+              <span class="text--disabled font-weight-500">
+                {{ getParseTime(item.date) }}
+              </span>
+            </v-col>
+            <v-col cols="12" sm="4" class="px-0">
+              <div class="d-flex justify-end align-center height-100">
+                <v-btn
+                  color="primary"
+                  elevation="0"
+                  class="px-0 mr-1"
+                  min-width="40"
+                  height="40"
+                  @click="download(item.path)"
+                >
+                  <v-icon size="20" color="cardBackground"
+                    >mdi-file-download-outline</v-icon
+                  >
+                </v-btn>
+
+                <v-menu left offset-x>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      v-bind="attrs"
+                      v-on="on"
+                      color="transparent"
+                      elevation="0"
+                      class="px-0"
+                      min-width="40"
+                      height="40"
+                    >
+                      <v-icon size="24" color="textGray"
+                        >mdi-dots-vertical</v-icon
+                      >
+                    </v-btn>
+                  </template>
+                  <v-list class="py-0 px-0 d-flex flex-column">
+                    <v-btn
+                      elevation="0"
+                      text
+                      class="height-100 px-3"
+                      height="40"
+                      color="text"
+                      ><v-icon class="mr-3" size="24" color="error"
+                        >mdi-alert-circle-outline</v-icon
+                      >Посмотреть ошибки</v-btn
+                    >
+                  </v-list>
+                </v-menu>
+              </div>
+            </v-col>
+          </v-row>
+        </template>
+        <template v-else>
+          <div
+            style="height: 100%"
+            class="d-flex justify-center align-center flex-column"
+          >
+            <v-icon size="100" class="mb-3 cursor-default" color="textGray"
+              >mdi-progress-download</v-icon
+            >
+            <span class="text--text text-h6 mb-3">Здесь пока пусто</span>
+            <span class="text--disabled text-h6"
+              >Выгрузки за данный период не обнаружено</span
+            >
+          </div>
+        </template>
+      </template>
+    </div>
     <!-- <v-card-title class="py-0 px-0 text--text text-h5 font-weight-bold"
       >Вывод ошибок</v-card-title
     > -->
@@ -52,11 +162,6 @@
         {{ item }}
       </v-row>
     </div> -->
-    <div>
-      <br />
-      <br />
-      <br />
-    </div>
     <v-divider class="mb-3"></v-divider>
     <v-row class="justify-end">
       <v-btn color="text" text> Закрыть </v-btn>
--- src/pages/personal/config/custom-personal-report/setup.js
+++ src/pages/personal/config/custom-personal-report/setup.js
@@ -1,6 +1,7 @@
-import { computed, defineComponent, ref } from 'vue'
+import Vue, { computed, defineComponent, ref, watch } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
 import moment from 'moment/moment'
+import store from '@/store'
 
 const PersonalReport = defineComponent({
   name: 'Отчет А/В',
@@ -9,8 +10,28 @@ const PersonalReport = defineComponent({
   components: {},
   props: {},
   setup(props, ctx) {
-    const menu = ref(false)
     const date = ref(moment(new Date()).format('YYYY-MM'))
+    const loading = ref({
+      period: false,
+      errors: false,
+    })
+    const data = ref({
+      period: [
+        {
+          success: true,
+          date: '2024-08-16 12:02:44',
+          path: '/tmp/1723809533-2024-07.xlsx',
+          parser_id: 182,
+        },
+        {
+          success: false,
+          date: '2024-08-16 12:14:57',
+          path: '/tmp/1723810350-2024-08.xlsx',
+          parser_id: 183,
+        },
+      ],
+      errors: [],
+    })
     const monthArray = [
       'Январь',
       'Февраль',
@@ -28,8 +49,8 @@ const PersonalReport = defineComponent({
     const getYear = (val) => {
       return moment(val, 'YYYY-MM').year()
     }
-    const getMonth = (val, x) => {
-      let monthIndex = moment(val, 'YYYY-MM').month()
+    const getMonth = (x) => {
+      let monthIndex = moment(date.value, 'YYYY-MM').month()
       if (x) {
         monthIndex += x
         if (monthIndex === -1) monthIndex = 11
@@ -37,13 +58,57 @@ const PersonalReport = defineComponent({
       }
       return monthArray[monthIndex]
     }
+    const getParseTime = (val) => {
+      return moment(val, 'YYYY-MM-DD HH:mm:ss').format('DD.MM.YYYY, HH:mm')
+    }
+    const changeMonth = (val) => {
+      date.value = moment(date.value, 'YYYY-MM').add(val, 'M').format('YYYY-MM')
+    }
+
+    let controller
+    const getData = async () => {
+      if (controller) controller.abort()
+      controller = new AbortController()
+      loading.value.period = true
+      const responseData = await store.dispatch('form/update', {
+        url: 'report/autoload/x5/info',
+        body: {
+          data: {
+            period: date.value,
+          },
+        },
+        params: {
+          signal: controller.signal,
+        },
+      })
+      // data.value.period = responseData.data
+      loading.value.period = false
+      controller = undefined
+    }
+
+    const download = (val) => {
+      Vue.downloadFile(val)
+    }
+
+    watch(
+      () => date.value,
+      () => {
+        getData()
+      },
+      { immediate: true }
+    )
+
     return {
-      menu,
       date,
       monthArray,
+      loading,
+      data,
 
       getYear,
       getMonth,
+      getParseTime,
+      changeMonth,
+      download,
     }
   },
 })
