0e53de75779850ee02ec8d25c23c3e69a30db829
Aleksey Tikhonravov <fosterfosterfoster63@gmail.com>
2024-09-05 17:49:46 +0400

    Поправил доступы

--- src/compositions/useForm.js
+++ src/compositions/useForm.js
@@ -1434,13 +1434,21 @@ export default function ({
   }
 
   const getDepFilters = (target) => {
+    console.log(target)
     if (!target.filter) return []
     const filters = target?.filter?.flatMap((el) => {
+      console.log(target.name, el.with_me)
       const filter = {
         alias: el.alias ?? el.field,
         type: el.type,
       }
-      if (!formData[el.field] && !el.source && !el.routeKey) return []
+      if (
+        !formData[el.field] &&
+        !el.source &&
+        !el.routeKey &&
+        !el.hasOwnProperty('with_me')
+      )
+        return []
       if (el.source) {
         if (el.source === 'fromPrev') {
           filter.value = form?.formData[el.field]
@@ -1462,6 +1470,9 @@ export default function ({
         }
       } else if (el.routeKey) {
         filter.value = +route.params[el.routeKey]
+      } else if (el.hasOwnProperty('with_me') && el.with_me === false) {
+        filter.value = el.with_me
+        filter.alias = 'with_me'
       } else {
         filter.value = formData[el.field]
         if (moment(filter.value, 'YYYY.MM', true).isValid())
